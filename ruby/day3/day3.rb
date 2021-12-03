c41ef7d92d613aba45213b4bfb712020a1e87631
Akash Kurdekar <akashkurdekar@gmail.com>
2021-12-03 22:00:30 +0000

    done; needs refactoring

--- ruby/day3/day3.rb
+++ ruby/day3/day3.rb
@@ -13,36 +13,53 @@ end
 
 def part_two()
     readings = read_input_file_lines()
-    readings = ["00100",
-    "11110",
-    "10110",
-    "10111",
-    "10101",
-    "01111",
-    "00111",
-    "11100",
-    "10000",
-    "11001",
-    "00010",
-    "01010"]
+    # readings = ["00100",
+    # "11110",
+    # "10110",
+    # "10111",
+    # "10101",
+    # "01111",
+    # "00111",
+    # "11100",
+    # "10000",
+    # "11001",
+    # "00010",
+    # "01010"]
 
     reduced = work_on(readings, 0)
-    return reduced
+
+    reduced2 = work_on2(readings, 0)
+    return bin_to_dec(reduced) * bin_to_dec(reduced2)
 end
 
 def work_on(readings, bit_index) 
-    puts "readings: #{readings}"
+    # puts "readings: #{readings}"
 
     if readings.length == 1
         return readings[0]
     end
 
     most_common = get_most_common_bits(readings)[bit_index]
-    puts "most_common: #{most_common}"
+    # puts "most_common: #{most_common}"
     filtered_readings = readings.filter { |r| r[bit_index] == most_common }
-    puts "filtered_readings: #{filtered_readings}"
+    # puts "filtered_readings: #{filtered_readings}"
 
     return work_on(filtered_readings, bit_index + 1)
+end
+
+def work_on2(readings, bit_index)
+    # puts "readings: #{readings}"
+
+    if readings.length == 1
+        return readings[0]
+    end
+
+    least_common = get_least_common_bits(readings)[bit_index]
+    # puts "least_common: #{least_common}"
+    filtered_readings = readings.filter { |r| r[bit_index] == least_common }
+    # puts "filtered_readings: #{filtered_readings}"
+    # puts "========="
+    return work_on2(filtered_readings, bit_index + 1)
 end
 
 
@@ -61,6 +78,18 @@ def get_most_common_bits(readings)
     
     most_common = find_most_common(positional_bits)
     return most_common
+end
+
+def get_least_common_bits(readings)
+    width = readings[0].length
+    positional_bits = []
+    (0..width-1).each do |i|
+        bap = get_bits_at_position(readings, i)
+        positional_bits.push(bap)
+    end
+    
+    least_common = find_least_common(positional_bits)
+    return least_common
 end
 
 def get_bits_at_position(readings, pos) 
@@ -83,6 +112,18 @@ def find_most_common(positional_bits)
     return most_common
 end
 
+def find_least_common(positional_bits) 
+    least_common = ""
+    positional_bits.each do |p| 
+        if p.count("0") * 2  <= p.length
+            least_common += "0"
+        else
+            least_common += "1"
+        end
+    end
+    return least_common
+end
+
 def invert(bits)
     inverted = bits.chars.map do |c| 
         c == "0" ? "1" : "0"
@@ -103,4 +144,4 @@ end
 
 puts "Part one: #{part_one()}"
 
-# puts "Part two: #{part_two()}"
+puts "Part two: #{part_two()}"
