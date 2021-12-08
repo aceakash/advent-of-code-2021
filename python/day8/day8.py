cadd3554a851c76e9454aa571602275997230b74
Akash Kurdekar <akashkurdekar@gmail.com>
2021-12-08 14:21:27 +0000

    refactor

--- python/day8/day8.py
+++ python/day8/day8.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def example2():
     return """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
@@ -15,7 +18,7 @@ def example1():
 
 
 def example3():
-    return """cf bcdf acf abcdefg abdfg acdfg acdeg abcdfg abcefg abdefg | acdeg acdfg acdeg acdfg"""
+    return """cf bcdf acf abcdefg abdfg acdfg acdeg abcdfg abcefg abdefg | acdeg acdfg abcdfg abdefg"""
 
 def real_input():
     with open("input.txt") as file:
@@ -24,47 +27,35 @@ def real_input():
 
 def part_one(input):
     # how many segments of 2,3,4,7 lengths?
-    items = parse_input(input)                    # ['line1', 'line2']
-    # print(items)
-    
+    items = parse_input(input)
     return count_1_4_7_8s(items)
 
 def part_two(input):
     items = parse_input(input)
-    total = 0
-    for item in items:
-        nums = extract_num(item)
-        total += nums
-
-    return total
+    nums = [extract_num(item) for item in items]
+    return sum(nums)
     
 def extract_num(item):
     lookup = make_lookup(item)
-    # lookup = {
-    #     'abcdefg': 8,
-    #     'bcdef': 5,
-    #     'acdfg': 2,
-    #     'abcdf': 3,
-    #     'abd': 7,
-    #     'abcdef': 9,
-    #     'bcdefg': 6,
-    #     'abef': 4,
-    #     'abcdeg': 0,
-    #     'ab': 1
-    # }
     strs = item['four_segments']
-    nums = ''
-    for s in strs:
-        s2 = ''.join(sorted(s))
-        num_str = lookup[s2]
-        nums += num_str
-    
-    return int(nums)
+    num_strs = [lookup[''.join(sorted(s))] for s in strs]
+    return int(''.join(num_strs))
 
 def make_lookup(item):
-    unknowns = []
-    lookups = {}
-    for s in item['ten_segments']:
+    unknowns, lookups = [], {}
+    
+    find_one_four_seven_eight(lookups, unknowns, item)
+    two_code = find_two(lookups, unknowns, item)
+    three_code = find_three(two_code, lookups, unknowns, item)
+    five_code = find_five(lookups, unknowns, item)
+    find_zero(five_code, lookups, unknowns, item)
+    find_nine(three_code, lookups, unknowns, item)
+    find_six(lookups, unknowns, item)
+
+    return lookups
+
+def find_one_four_seven_eight(lookups, unknowns, item):
+     for s in item['ten_segments']:
         s2 = ''.join(sorted(s))
         if len(s2) == 2:
             lookups[s2] = '1'
@@ -79,12 +70,34 @@ def make_lookup(item):
             lookups[s2] = '4'
             continue
         unknowns.append(s2)
+    
 
-    two_code = find_two(lookups, unknowns, item)
-    find_three(two_code, lookups, unknowns, item)
-    print(lookups)
-    print(unknowns)
-    return lookups
+def find_six(lookups, unknowns, item):
+    lookups[unknowns[0]] = '6'
+    unknowns.remove(unknowns[0])
+
+def is_subset_of(a, b):
+    return len(set(a).difference(set(b))) == 0
+
+def find_nine(three_code, lookups, unknowns, item):
+    # nine is the only one containing three_code as a subset
+    nine_code = [x for x in unknowns if len(x) == 6 and is_subset_of(three_code, x)][0]
+    lookups[nine_code] = '9'
+    unknowns.remove(nine_code)
+
+def find_zero(five_code, lookups, unknowns, item):
+    # zero is the only one which doesnt contain five_code as a subset
+    zero_code = [x for x in unknowns if len(x) == 6 and not is_subset_of(five_code, x)][0]
+    lookups[zero_code] = '0'
+    unknowns.remove(zero_code)
+
+
+def find_five(lookups, unknowns, item):
+    # five is the only five-letter code remaining
+    five_code = [x for x in unknowns if len(x) == 5][0]
+    lookups[five_code] = '5'
+    unknowns.remove(five_code)
+    return five_code
 
 def find_two(lookups, unknowns, item):
     # the code for two is not a subset of any other unknowns
@@ -95,80 +108,49 @@ def find_two(lookups, unknowns, item):
     for fl in five_letters:
         found_subset = False
         for sl in six_letters:
-            if is_subset(fl, sl):
+            if is_subset_of(fl, sl):
                 found_subset = True 
                 break
         if not found_subset:
             two_code = fl
             break
 
-    if two_code == None:
-        print('code for two was not found. there is a prob with the code')
-        exit(1)
-    
     lookups[two_code] = '2'
     unknowns.remove(two_code)
     return two_code
 
 def find_three(two_code, lookups, unknowns, item):
     # the code for three is one letter off from the code for two
-    five_letters = [x for x in unknowns if len(x) == 5]
-    print("===" ,five_letters)
-
-    three_code = None
-    
-    for fl in five_letters:
-        if len(set(fl).difference(set(two_code))) == 1:
-            three_code = fl
-            break
-
-
-
-    if three_code == None:
-        print('code for three was not found. there is a prob with the code')
-        exit(1)
-    
+    three_code = [x for x in unknowns if len(x) == 5 and has_one_difference(x, two_code)][0]
     lookups[three_code] = '3'
     unknowns.remove(three_code)
+    return three_code
 
-
-def is_subset(needle, haystack):
-    unique = set(needle).difference(set(haystack))
-    return len(unique) == 0
-
+def has_one_difference(a, b):
+    return len(set(a).difference(set(b))) == 1
 
 def parse_input(input):
-    lines = input.split('\n')
     items = []
-    for l in lines:
+    for l in input.split('\n'):
         parts = l.strip().split('|')
-        ten_segments = parts[0].strip().split(' ')
-        four_segments = parts[1].strip().split(' ')
-        
-        item = {
-            'raw_line': l,
-            'ten_segments': ten_segments,
-            'four_segments': four_segments 
-        }
-        items.append(item)
-
+        items.append({
+            'ten_segments': parts[0].strip().split(' '),
+            'four_segments': parts[1].strip().split(' ') 
+        })
     return items
 
 
 def count_1_4_7_8s(items):
     count = 0
     for item in items:
-        strs = item['four_segments']
-        for s in strs:
-            if len(s) == 2 or len(s) == 4 or len(s) == 3 or len(s) == 7:
+        for s in item['four_segments']:
+            if len(s) in [2,4,3,7]:
                 count += 1
     
     return count
 
 
 if __name__ == "__main__":
-    # print(part_one(real_input()))
-
-    print(part_two(example3()))
-    # print(part_two(example2()))
-    # print(part_two(real_input()))
+    assert part_one(real_input()) == 539 
+    assert part_two(real_input()) == 1084606 
+    
