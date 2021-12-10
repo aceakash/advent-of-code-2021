d267aac3064bafb39b2c31a255de8714ebceb75d
Akash K <akashkurdekar@gmail.com>
2021-12-10 09:37:27 +0000

    day10 part one solved

--- go/day10/chunks.go
+++ go/day10/chunks.go
@@ -1,24 +1,46 @@
 package day10
 
 import (
-	"fmt"
 	"github.com/aceakash/advent-of-code-2021/day10/stack"
 	"strings"
 )
 
-func FindFirstWrongCloser(input string) string {
-	fmt.Println(input)
-	if len(strings.TrimSpace(input)) == 0 {
+func PartOne(input string) int {
+	lines := parseLines(input)
+
+	totalSyntaxErrorScore := 0
+	for _, line := range(lines) {
+		sec := syntaxErrorScore(line)
+		totalSyntaxErrorScore += sec
+	}
+	return totalSyntaxErrorScore
+}
+
+func parseLines(input string) []string {
+	return strings.Split(input, "\n")
+}
+
+func syntaxErrorScore(line string) int {
+	wrong := FindFirstWrongCloser(line)
+	if wrong == "" {
+		return 0
+	}
+	scores := map[string]int {
+		")": 3,
+		"]": 57,
+		"}": 1197,
+		">": 25137,
+	}
+	return scores[wrong]
+}
+
+func FindFirstWrongCloser(line string) string {
+	if len(strings.TrimSpace(line)) == 0 {
 		return ""
 	}
-
-	stringStack := stack.NewStringStack()
-	fmt.Println("stringStack when created", stringStack)
-	firstWrongCloser := ""
-	for _, r := range input {
+	stringStack, firstWrongCloser := stack.NewStringStack(), ""
+	for _, r := range line {
 		sr := string(r)
-		fmt.Println("sr", sr)
-		fmt.Println("stringStack", stringStack)
 		if isOpening(sr) {
 			stringStack.Push(sr)
 			continue
@@ -29,24 +51,45 @@ func FindFirstWrongCloser(input string) string {
 				firstWrongCloser = sr
 				break
 			}
-			if popped != sr {
+			if !isClosingFor(sr, popped) {
 				firstWrongCloser = sr
 				break
 			}
-			continue
 		}
 	}
-
 	return firstWrongCloser
 }
 
+// ----
+var openingToClosingMap = map[string]string{
+	"(": ")",
+	"{": "}",
+	"<": ">",
+	"[": "]",
+}
+
 func isClosing(sr string) bool {
-	return strings.Contains("}])>", sr)
+	for _, v := range openingToClosingMap {
+		if v == sr {
+			return true
+		}
+	}
+	return false
+}
+
+func isClosingFor(closing string, opening string) bool {
+	c, ok := openingToClosingMap[opening]
+	if ok && c == closing {
+		return true
+	}
+	return false
 }
 
 func isOpening(sr string) bool {
-	return strings.Contains("({[<", sr)
+	for k, _ := range openingToClosingMap {
+		if k == sr {
+			return true
+		}
+	}
+	return false
 }
-
-
-
--- typescript/day5/tests/hello.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import {hello} from '../hello'
-
-describe('hello!', () => { 
-    it('works', () => { 
-        expect(hello('akash')).to.equal('hello akash!')
-    });
-});
-
